40d6b6a33e3d9b51749d810d156dd0a23ad83e13
kevinG73 <kevinguelade@gmail.com>
2021-02-12 10:56:27 +0000

    UPD fetch_departement.php and fetch_parcours.php

--- ajax/fetch_departement.php
+++ ajax/fetch_departement.php
@@ -5,12 +5,8 @@ require "../fonctions/index.php";
 if (isset($_GET['id_etablissement']) && !empty($_GET['id_etablissement'])):
     $id_etablissement = (int)$_GET['id_etablissement'];
     global $bdd;
-    $requete = "SELECT * FROM departement WHERE id_etablissement = $id_etablissement AND id_departement <> 39 AND id_departement <> 40
-    AND id_departement <> 41 AND id_departement <> 52 AND id_departement <> 53
-    AND id_departement <> 28 AND id_departement <> 29 AND id_departement <> 30 AND id_departement <> 31
-    AND id_departement <> 32 AND id_departement <> 33 AND id_departement <> 34 AND id_departement <> 38
-    AND id_departement <> 25 AND id_departement <> 26 AND id_departement <> 27 AND id_departement <> 91
-    AND id_departement <> 36" ;
+    $requete = "SELECT * FROM departement WHERE id_etablissement = $id_etablissement AND id_departement NOT IN (39 , 40 , 41 , 52 , 53 , 28 , 29 , 30 , 
+    31 ,32 , 33 , 34 , 38 , 25 , 26 , 27, 91 , 36)";
 
     $resultat = $bdd->query($requete);
     $liste = $resultat->fetchAll();
--- ajax/fetch_parcours.php
+++ ajax/fetch_parcours.php
@@ -2,9 +2,8 @@
 require "../config/connexion.php";
 require "../fonctions/index.php";
 
-if (isset($_GET['id_departement']) && !empty($_GET['id_niveau'])):
+if (isset($_GET['id_departement'])):
     $id_departement = $_GET['id_departement'];
-    $id_niveau = $_GET['id_niveau'];
     global $bdd;
     $requete = "SELECT specialite.id_specialite ,specialite.libelle_specialite 
                 FROM specialite,composer_maquette,mention,semestre
@@ -12,7 +11,7 @@ if (isset($_GET['id_departement']) && !empty($_GET['id_niveau'])):
                 specialite.id_specialite = composer_maquette.id_specialite
                 and specialite.id_mention = mention.id_mention
                 and composer_maquette.id_semestre = semestre.id_semestre
-                and semestre.id_niveau = '" . $id_niveau . "' and mention.id_departement = '" . $id_departement . "' 
+                and semestre.id_niveau = 3 and mention.id_departement = '" . $id_departement . "' 
                 group by specialite.id_specialite";
 
     $resultat = $bdd->query($requete);
