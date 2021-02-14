aafa56de6da213cd23aa85cb93e9fda1cd0b9375
Christianzer <christianaka65@gmail.com>
2021-02-14 14:01:57 +0000

    priseEnCompteDesConnexions

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,10 +1,28 @@
+<?php
+require 'config/connexion.php';
+$info = $bdd->query('select * from utilisateur where id_utilisateur ="'.$_SESSION['connecte'].'" ');
+$information = $info->fetch();
+if ($_SESSION['id_type_utilisateur '] == 1){
+
+}else{
+    $nom_etablissement = $bdd->query("SELECT nom_etablissement FROM etablissement WHERE id_etablissement = '".$_SESSION['id_etablissement']."'")->fetchColumn();
+    $nom_departement = $bdd->query("SELECT nom_departement FROM departement WHERE id_departement = '".$_SESSION['id_departement']."'")->fetchColumn();
+}
+
+?>
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+    <br>
+    <div class="col-md-5">
+        <p class="mb-3"><h6 class="text-uppercase">NOM ET PRENOMS : <?=$information['nom_utilisateur'].' '.$information['prenom_utilisateur']?></h6></p>
+        <?php
 
-    <!-- Sidebar Toggle (Topbar) -->
-    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-        <i class="fa fa-bars"></i>
-    </button>
+        ?>
+        <p class="mb-3"><h6 class="text-uppercase">UFR : <?php echo @$nom_etablissement?></h6></p>
+    </div>
 
+    <div class="col-md-5">
+        <p class="mb-5"> <h6 class="text-uppercase">UNITE DE FORMATION : <?php echo @$nom_departement?></h6></p>
+    </div>
 
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
